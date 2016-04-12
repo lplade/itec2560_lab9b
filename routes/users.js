978cc4ae775039b85bc5c4e3a29b5e1b2155467e
Larry Lade <lplade@users.noreply.github.com>
2016-04-11 23:17:14 -0500

    Troubleshooting delete function

--- routes/users.js
+++ routes/users.js
@@ -26,4 +26,16 @@ router.post('/adduser', function(req, res) {
     });
 });
 
+/*
+ * DELETE to deleteuser
+ */
+router.delete('/deleteuser/:id', function(req, res){
+  var db = req.db;
+  var collection = db.get('userlist');
+  var userToDelete = req.params.id;
+  colleciton.remove({ '_id' : userToDelete }, function(err) {
+    res.send((err === null) ? { msg: '' } : { msg:'error: ' + err});
+  });
+});
+
 module.exports = router;
